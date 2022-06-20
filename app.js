a9ec885c97f28d5de8a1a00d1e15779c1813c2ed
Hexan Rauf <hexanraa@gmail.com>
2022-06-20 22:55:36 +0500

    Created a function to play one round

--- app.js
+++ app.js
@@ -4,7 +4,42 @@ let playerScore = 0;
 let computerScore = 0;
 
 // Selecting a Random Choice
-function computerPlay()={
-  const randomChoice= choice[Math.floor(Math.random()* choices.length)];
+function computerPlay() {
+  const randomChoice = choices[Math.floor(Math.random() * choices.length)];
   return randomChoice;
+}
+
+// Single Round of Rock Paper Scissors
+
+function playRound(playerSelection, computerSelection) {
+  // Player choice is Rock
+  if (playerSelection === "rock" && computerSelection === "Rock") {
+    console.log("You Drew! Rock equals Rock");
+  } else if (playerSelection === "rock" && computerSelection === "Paper") {
+    computerScore++;
+    console.log("You Lose! Paper beats Rock");
+  } else if (playerSelection === "rock" && computerSelection === "Scissors") {
+    playerScore++;
+    console.log("You Win! Rock beats Scissors");
+  }
+  // Player choice is Paper
+  else if (playerSelection === "paper" && computerSelection === "Paper") {
+    console.log("You Drew! Paper equals Paper");
+  } else if (playerSelection === "paper" && computerSelection === "Scissors") {
+    computerScore++;
+    console.log("You Lose! Scissors beats Paper");
+  } else if (playerSelection === "paper" && computerSelection === "Rock") {
+    playerScore++;
+    console.log("You Win! Paper beats Rock");
+  }
+  // Player choice is Scissors
+  else if (playerSelection === "scissors" && computerSelection === "Scissors") {
+    console.log("You Drew! Scissors equals Scissors");
+  } else if (playerSelection === "scissors" && computerSelection === "Rock") {
+    computerScore++;
+    console.log("You Lose! Rock beats Scissors");
+  } else if (playerSelection === "scissors" && computerSelection === "Paper") {
+    playerScore++;
+    console.log("You Win! Scissors beats Paper");
+  }
 }
